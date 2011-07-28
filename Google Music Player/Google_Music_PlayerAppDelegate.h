fe6d689eeda51115c62894138c665bc50946cdf5
Julius Parishy <juliusparishy@gmail.com>
2011-07-28 19:01:29 -0400

    Initial Commit

---
+++ Google Music Player/Google_Music_PlayerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Google_Music_PlayerAppDelegate.h
+//  Google Music Player
+//
+//  Created by Julius Parishy on 7/28/11.
+//  Copyright 2011 DiDev Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Google_Music_PlayerAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Google Music Player/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Google Music Player
+//
+//  Created by Julius Parishy on 7/28/11.
+//  Copyright 2011 DiDev Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end

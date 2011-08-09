a3b1be9f288e981170293b049fcc6cab82e358dd
Julius Parishy <juliusparishy@gmail.com>
2011-08-09 17:38:16 -0400

    Added preliminary support for playback control and navigation flow for Artists, Albums, and songs

---
+++ Google Music Player/ArtistAlbumsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AristAlbumsViewController.h
+//  Google Music Player
+//
+//  Created by Julius Parishy on 8/7/11.
+//  Copyright 2011 DiDev Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AristAlbumsViewController : UITableViewController
+
+@end

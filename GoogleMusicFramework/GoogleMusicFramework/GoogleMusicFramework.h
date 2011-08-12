ed5fd5b027447de9c3ab947a4137da2040cb3aaf
Julius Parishy <juliusparishy@gmail.com>
2011-08-12 15:03:49 -0400

    Some structuring, getting rid of unnecessary files. Starting the move to separating the Google Music code from the interface so it can be released at some point.

---
+++ GoogleMusicFramework/GoogleMusicFramework/GoogleMusicFramework.h
@@ -0,0 +1,13 @@
+//
+//  GoogleMusicFramework.h
+//  GoogleMusicFramework
+//
+//  Created by Julius Parishy on 8/12/11.
+//  Copyright 2011 DiDev Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleMusicFramework : NSObject
+
+@end
---
+++ GoogleMusicFramework/GoogleMusicFramework/GoogleMusicFramework.m
@@ -0,0 +1,23 @@
+//
+//  GoogleMusicFramework.m
+//  GoogleMusicFramework
+//
+//  Created by Julius Parishy on 8/12/11.
+//  Copyright 2011 DiDev Studios. All rights reserved.
+//
+
+#import "GoogleMusicFramework.h"
+
+@implementation GoogleMusicFramework
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
